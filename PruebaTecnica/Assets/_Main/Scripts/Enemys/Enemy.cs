69100c85cb857f5658726ce58a087508785ade41
juananre <78058130+juananre@users.noreply.github.com>
2024-07-07 17:02:47 -0500

    Problems
    problemas de movimiento del enemigo y animacion

--- PruebaTecnica/Assets/_Main/Scripts/Enemys/Enemy.cs
+++ PruebaTecnica/Assets/_Main/Scripts/Enemys/Enemy.cs
@@ -3,6 +3,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, IEnemy
 {
     [SerializeField] private int health = 10;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Animator ani;
+    [SerializeField] private float velocity = 2f;
+
+    void Start()
+    {
+        if (ani == null)
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            WalkNPC();
+        }
+    }
 
     public void TakeDamage(int amount)
     {
@@ -13,5 +37,16 @@ public class Enemy : MonoBehaviour, IEnemy
         {
             Destroy(gameObject);
         }
+    }
+
+    void WalkNPC()
+    {
+        Vector3 lookPos = player.transform.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotacion = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotacion, 2);
+
+        ani.SetBool("Walk", true);
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
     }
 }
