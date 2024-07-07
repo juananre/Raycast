265a03de823c6771535943a0835da4f31ad0d515
juananre <78058130+juananre@users.noreply.github.com>
2024-07-07 13:37:15 -0500

    PlayerMovment
    He inpremetado el movimieto del personaje principal en PlayerCamController, al igual sentar bases para el laser o blaster usando la samplescene para testear

---
+++ PruebaTecnica/Assets/_Main/Scripts/Blast_Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blast_Controller : MonoBehaviour
+{
+    [SerializeField] private Transform ShootPosition;
+    private float LaserRange = 100f, LaserDuration = 0.06f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PruebaTecnica/Assets/_Main/Scripts/PlayerCamController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamController : MonoBehaviour
+{
+    private float mouseX, mouseY;
+    [SerializeField] float velocity = 100f;
+    float rotatioinX = 0f;
+    [SerializeField] Transform Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX= Input.GetAxis("Mouse X") * velocity * Time.deltaTime;
+        mouseY= Input.GetAxis("Mouse Y") * velocity * Time.deltaTime;
+
+        rotatioinX -= mouseY;
+        rotatioinX = Mathf.Clamp(rotatioinX,-90f, 90f);
+
+        transform.localRotation= Quaternion.Euler(rotatioinX,0f,0f);
+        Player.Rotate(Vector3.up * mouseX);
+    }
+}
