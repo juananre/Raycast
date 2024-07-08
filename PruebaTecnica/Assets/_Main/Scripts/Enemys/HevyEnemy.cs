412b4449e07fa7a456400ad941e0773e85c85e4f
juananre <78058130+juananre@users.noreply.github.com>
2024-07-07 22:15:00 -0500

    MapLevel

---
+++ PruebaTecnica/Assets/_Main/Scripts/Enemys/HevyEnemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HevyEnemy : BaseEnemy, IEnemy
+{
+    [SerializeField] private new int health = 2;
+    [SerializeField] private new float velocity = 5;
+    [SerializeField] private new float velocityAnimation = 2f;
+    [SerializeField] private Color typeEnemy;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        Debug.Log("Enemigo recibió daño, salud restante: " + health);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected override Color GetColor()
+    {
+        return typeEnemy;
+    }
+}
